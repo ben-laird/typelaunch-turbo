39a3ff8f36eadf3a96c6b66af12c60d01e96133e
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-20 18:39:28 -0400

    test: :white_check_mark: update tests with fast-check/vitest

--- templates/tlt-external-vite/test/index.test.ts
+++ templates/tlt-external-vite/test/index.test.ts
@@ -1,16 +1,11 @@
-import { describe, it } from "vitest";
-import fc from "fast-check";
+import { describe } from "vitest";
+import { fc, test } from "@fast-check/vitest";
 
 import { add } from "../src";
 
 describe("Math Helpers tests", () => {
-  it("Should add two numbers", () => {
-    fc.assert(
-      fc.property(
-        fc.integer(),
-        fc.integer(),
-        (number1, number2) => add(number1, number2) === number1 + number2
-      )
-    );
-  });
+  test.prop([fc.integer(), fc.integer()])(
+    "Should add two numbers",
+    (number1, number2) => add(number1, number2) === number1 + number2
+  );
 });
--- templates/tlt-internal/test/sanity.test.ts
+++ templates/tlt-internal/test/sanity.test.ts
@@ -1,5 +1,11 @@
-import { describe, it } from "vitest";
+import { fc, test } from "@fast-check/vitest";
+import { assert, describe, it } from "vitest";
 
 describe("Passing sanity test", () => {
-  it("Should just pass");
+  it("Should just pass", () => assert(1 === 1));
+
+  test.prop([fc.float({ noNaN: true })])(
+    "Should also pass",
+    (num) => num === num
+  );
 });
