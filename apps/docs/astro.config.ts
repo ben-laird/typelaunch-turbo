36a9d9cd5afbe46131905fcfe9308cf6a72d7329
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-05-12 19:56:58 -0500

    feat: renovate docs
    Use Astro docs template

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -1,7 +1,14 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+  ],
+  site: "https://astro.build",
 });
