4be406a902ebd01be258a954eeeffdd3b39244f3
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-06-16 02:00:19 -0500

    docs: switch to astrojs/starlight

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
   ],
-  site: "https://astro.build",
 });
